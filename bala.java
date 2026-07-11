39b95d8b7daad8f05f2f580cdf881ec32658640f
konidenabalasindhu <konidenabalasindhu2307@gmail.com>
2026-07-11 15:49:01 +0530

    Add files via upload

---
+++ bala.java
@@ -0,0 +1,15 @@
+public class bala {
+
+public static void main (String args[]){
+
+System.out.println("Hello world.");
+
+System.out.println("Name:Konidena bala sindhu.");
+
+System.out.println("Rollno:25AG1A66F5.");
+
+System.out.println("Section:CSE(AI/ML).");
+
+}
+
+}
